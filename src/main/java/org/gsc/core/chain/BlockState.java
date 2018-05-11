942aed50b4103d176e16fe61a7606cf7eef482ec
EvanYip <contact@gsc.social>
2018-05-11 21:18:18 +0800

    add block header and block wrapper

--- src/main/java/org/gsc/core/chain/BlockState.java
+++ src/main/java/org/gsc/core/chain/BlockState.java
@@ -1,6 +1,22 @@
 package org.gsc.core.chain;
 
-public class BlockState {
+import com.google.protobuf.ByteString;
+import com.google.protobuf.InvalidProtocolBufferException;
+import java.security.SignatureException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Vector;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import org.apache.commons.collections4.CollectionUtils;
+import org.gsc.common.exception.BadItemException;
+import org.gsc.common.exception.ValidateSignatureException;
+import org.gsc.common.utils.ByteUtil;
+import org.gsc.common.utils.Sha256Hash;
+import org.gsc.crypto.ECKey;
 
-  private Block block;
+public class BlockState extends BlockHeaderState{
+
+
+
 }
