420fd0df28d3842f63fb2ffbb7b5bc663cc04561
EvanYip <contact@gsc.social>
2018-05-11 16:45:27 +0800

    add producer service

---
+++ src/main/java/org/gsc/service/ProducerService.java
@@ -0,0 +1,28 @@
+package org.gsc.service;
+
+import org.gsc.config.Args;
+
+public class ProducerService implements Service{
+
+  @Override
+  public void init() {
+
+  }
+
+  @Override
+  public void init(Args args) {
+
+  }
+
+  @Override
+  public void start() {
+
+  }
+
+  @Override
+  public void stop() {
+
+  }
+
+
+}
