9ca1d5b75ae163c20733befce2e87030c1ef7059
EvanYip <contact@gsc.social>
2018-05-22 11:20:18 +0800

    add base 58

--- src/main/java/org/gsc/common/utils/AddressUtil.java
+++ src/main/java/org/gsc/common/utils/AddressUtil.java
@@ -1,7 +1,10 @@
 package org.gsc.common.utils;
 
+import static org.gsc.config.Parameter.WalletConstant.BASE58CHECK_ADDRESS_SIZE;
+
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.gsc.config.Parameter.WalletConstant;
 
 @Slf4j
@@ -25,6 +28,42 @@ public class AddressUtil {
     }
     //Other rule;
     return true;
+  }
+
+  public static byte[] decodeFromBase58Check(String addressBase58) {
+    if (StringUtils.isEmpty(addressBase58)) {
+      logger.warn("Warning: Address is empty !!");
+      return null;
+    }
+    if (addressBase58.length() != BASE58CHECK_ADDRESS_SIZE) {
+      logger.warn(
+          "Warning: Base58 address length need " + BASE58CHECK_ADDRESS_SIZE + " but "
+              + addressBase58.length()
+              + " !!");
+      return null;
+    }
+    byte[] address = decode58Check(addressBase58);
+    if (!addressValid(address)) {
+      return null;
+    }
+    return address;
+  }
+
+  private static byte[] decode58Check(String input) {
+    byte[] decodeCheck = Base58.decode(input);
+    if (decodeCheck.length <= 4) {
+      return null;
+    }
+    byte[] decodeData = new byte[decodeCheck.length - 4];
+    System.arraycopy(decodeCheck, 0, decodeData, 0, decodeData.length);
+    byte[] hash1 = Sha256Hash.hashTwice(decodeCheck);
+    if (hash1[0] == decodeCheck[decodeData.length] &&
+        hash1[1] == decodeCheck[decodeData.length + 1] &&
+        hash1[2] == decodeCheck[decodeData.length + 2] &&
+        hash1[3] == decodeCheck[decodeData.length + 3]) {
+      return decodeData;
+    }
+    return null;
   }
 
 }
