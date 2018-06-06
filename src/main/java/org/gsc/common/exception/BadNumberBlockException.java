59184c6026a036c942b444062657699dca08617c
EvanYip <contact@gsc.social>
2018-06-06 20:05:32 +0800

    add exception

---
+++ src/main/java/org/gsc/common/exception/BadNumberBlockException.java
@@ -0,0 +1,12 @@
+package org.gsc.common.exception;
+
+public class BadNumberBlockException extends GscException {
+
+  public BadNumberBlockException() {
+    super();
+  }
+
+  public BadNumberBlockException(String message) {
+    super(message);
+  }
+}
