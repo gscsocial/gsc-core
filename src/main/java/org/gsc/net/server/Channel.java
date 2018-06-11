f6e89460d9a9f48d05b9cc607a9ad982723d4936
EvanYip <contact@gsc.social>
2018-06-11 20:17:57 +0800

    add pb varint decoder

--- src/main/java/org/gsc/net/server/Channel.java
+++ src/main/java/org/gsc/net/server/Channel.java
@@ -95,7 +95,7 @@ public class Channel {
         pipeline.addLast("readTimeoutHandler", new ReadTimeoutHandler(60, TimeUnit.SECONDS));
         pipeline.addLast(stats.tcp);
         pipeline.addLast("protoPender", new ProtobufVarint32LengthFieldPrepender());
-        pipeline.addLast("lengthDecode", new TrxProtobufVarint32FrameDecoder(this));
+        pipeline.addLast("lengthDecode", new GscProtobufVarint32FrameDecoder(this));
 
         //handshake first
         pipeline.addLast("handshakeHandler", handshakeHandler);
