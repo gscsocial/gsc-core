d9f3cbf256edeada347a214d62475b6ec8c2b5dd
EvanYip <contact@gsc.social>
2018-06-12 20:55:55 +0800

    add block message

--- src/main/java/org/gsc/net/message/gsc/BlockMessage.java
+++ src/main/java/org/gsc/net/message/gsc/BlockMessage.java
@@ -1,5 +1,57 @@
 package org.gsc.net.message.gsc;
 
-public class BlockMessage extends GscMessage{
+import org.gsc.common.exception.BadItemException;
+import org.gsc.common.utils.Sha256Hash;
+import org.gsc.core.chain.BlockId;
+import org.gsc.core.wrapper.BlockWrapper;
+import org.gsc.net.message.MessageTypes;
 
+public class BlockMessage extends GscMessage{
+  private BlockWrapper block;
+
+  public BlockMessage(byte[] data) throws BadItemException {
+    this.type = MessageTypes.BLOCK.asByte();
+    this.data = data;
+    this.block = new BlockWrapper(data);
+  }
+
+  public BlockMessage(BlockWrapper block) {
+    data = block.getData();
+    this.type = MessageTypes.BLOCK.asByte();
+    this.block = block;
+  }
+
+  public BlockId getBlockId() {
+    return getBlockCapsule().getBlockId();
+  }
+
+  public BlockWrapper getBlockCapsule() {
+    return block;
+  }
+
+  @Override
+  public Class<?> getAnswerMessage() {
+    return null;
+  }
+
+  @Override
+  public Sha256Hash getMessageId() {
+    return getBlockCapsule().getBlockId();
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    return super.equals(obj);
+  }
+
+  @Override
+  public int hashCode() {
+    return super.hashCode();
+  }
+
+  @Override
+  public String toString() {
+    return new StringBuilder().append(super.toString()).append(block.getBlockId().getString())
+        .append(", trx size: ").append(block.getTransactions().size()).append("\n").toString();
+  }
 }
