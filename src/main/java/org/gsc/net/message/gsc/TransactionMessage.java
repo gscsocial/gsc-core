e5371e6b63a625110bb408cadc830b3b8a018225
EvanYip <contact@gsc.social>
2018-06-13 19:44:59 +0800

    add tx message

--- src/main/java/org/gsc/net/message/gsc/TransactionMessage.java
+++ src/main/java/org/gsc/net/message/gsc/TransactionMessage.java
@@ -1,5 +1,44 @@
 package org.gsc.net.message.gsc;
 
-public class TransactionMessage  extends GscMessage {
+import org.gsc.common.exception.BadItemException;
+import org.gsc.common.utils.Sha256Hash;
+import org.gsc.core.wrapper.TransactionWrapper;
+import org.gsc.net.message.MessageTypes;
+import org.gsc.protos.Protocol.Transaction;
 
+public class TransactionMessage extends GscMessage {
+
+  private TransactionWrapper tx;
+
+  public TransactionMessage(byte[] data) throws BadItemException {
+    this.tx = new TransactionWrapper(data);
+    this.data = data;
+    this.type = MessageTypes.TRANSACTION.asByte();
+  }
+
+  public TransactionMessage(Transaction trx) {
+    this.tx = new TransactionWrapper(trx);
+    this.type = MessageTypes.TRANSACTION.asByte();
+    this.data = trx.toByteArray();
+  }
+
+  @Override
+  public String toString() {
+    return new StringBuilder().append(super.toString())
+        .append("messageId: ").append(super.getMessageId()).toString();
+  }
+
+  @Override
+  public Sha256Hash getMessageId() {
+    return this.tx.getTransactionId();
+  }
+
+  @Override
+  public Class<?> getAnswerMessage() {
+    return null;
+  }
+
+  public TransactionWrapper getTransactionWrapper() {
+    return this.tx;
+  }
 }
