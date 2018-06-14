94dc580344524cdda129e4bb43d4452b9ee7765a
EvanYip <contact@gsc.social>
2018-06-14 21:17:47 +0800

    add attention message

--- src/main/java/org/gsc/net/message/gsc/AttentionMessage.java
+++ src/main/java/org/gsc/net/message/gsc/AttentionMessage.java
@@ -1,5 +1,77 @@
 package org.gsc.net.message.gsc;
 
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.gsc.core.chain.BlockId;
+import org.gsc.net.message.MessageTypes;
+import org.gsc.protos.Protocol;
+import org.gsc.protos.Protocol.ChainInventory;
+
 public class AttentionMessage extends GscMessage {
 
+  protected ChainInventory chainInventory;
+
+  public AttentionMessage(byte[] data) throws Exception {
+    this.type = MessageTypes.ATTENTION.asByte();
+    this.data = data;
+    chainInventory = Protocol.ChainInventory.parseFrom(data);
+  }
+
+  public AttentionMessage(List<BlockId> blockIds, Long remainNum) {
+    ChainInventory.Builder invBuilder = ChainInventory.newBuilder();
+    blockIds.forEach(blockId -> {
+      Protocol.BlockId.Builder b = Protocol.BlockId.newBuilder();
+      b.setHash(blockId.getByteString());
+      b.setNumber(blockId.getNum());
+      invBuilder.addIds(b);
+    });
+
+    invBuilder.setRemainNum(remainNum);
+    chainInventory = invBuilder.build();
+    this.type = MessageTypes.ATTENTION.asByte();
+    this.data = chainInventory.toByteArray();
+  }
+
+  @Override
+  public Class<?> getAnswerMessage() {
+    return null;
+  }
+
+  private ChainInventory getChainInventory() {
+    return chainInventory;
+  }
+
+  public List<BlockId> getBlockIds() {
+
+    try {
+      return getChainInventory().getIdsList().stream()
+          .map(blockId -> new BlockId(blockId.getHash(), blockId.getNumber()))
+          .collect(Collectors.toCollection(ArrayList::new));
+    } catch (Exception e) {
+      logger.info("breakPoint");
+    }
+    return null;
+  }
+
+  public Long getRemainNum() {
+    return getChainInventory().getRemainNum();
+  }
+
+  @Override
+  public String toString() {
+    Deque<BlockId> blockIdWeGet = new LinkedList<>(getBlockIds());
+    StringBuilder sb = new StringBuilder(super.toString());
+    int size = blockIdWeGet.size();
+    sb.append("size: ").append(size);
+    if (size >= 1) {
+      sb.append(", first blockId: ").append(blockIdWeGet.peek().getString());
+      if (size > 1) {
+        sb.append(", end blockId: ").append(blockIdWeGet.peekLast().getString());
+      }
+    }
+    return sb.toString();
+  }
 }
