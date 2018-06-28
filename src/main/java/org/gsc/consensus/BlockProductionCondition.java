babcff6c32beaa3aa9afe135ac543b08d2919820
EvanYip <contact@gsc.social>
2018-06-28 20:19:28 +0800

    add backup cond

--- src/main/java/org/gsc/consensus/BlockProductionCondition.java
+++ src/main/java/org/gsc/consensus/BlockProductionCondition.java
@@ -1,14 +1,16 @@
 package org.gsc.consensus;
 
-public enum BlockProductionCondition {
+public enum  BlockProductionCondition {
   PRODUCED,           // Successfully generated block
-  NOT_SYNCED,
+  UNELECTED,
   NOT_MY_TURN,        // It isn't my turn
+  NOT_SYNCED,
   NOT_TIME_YET,       // Not yet arrived
   NO_PRIVATE_KEY,
   LOW_PARTICIPATION,
   LAG,
   CONSECUTIVE,
   TIME_OUT,
+  BACKUP_STATUS_IS_NOT_MASTER,
   EXCEPTION_PRODUCING_BLOCK
 }
