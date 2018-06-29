43d99d4aa7cfe0997d5b192b341482689a8f7779
EvanYip <contact@gsc.social>
2018-06-29 20:01:05 +0800

    add handle block method

--- src/main/java/org/gsc/core/sync/ChainControllerImpl.java
+++ src/main/java/org/gsc/core/sync/ChainControllerImpl.java
@@ -1,30 +1,93 @@
 package org.gsc.core.sync;
 
+import static org.gsc.config.GscConstants.ChainConstant.BLOCK_PRODUCED_INTERVAL;
+import static org.gsc.config.GscConstants.ChainConstant.BLOCK_SIZE;
+
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
+import org.gsc.common.exception.AccountResourceInsufficientException;
 import org.gsc.common.exception.BadBlockException;
+import org.gsc.common.exception.BadNumberBlockException;
 import org.gsc.common.exception.BadTransactionException;
+import org.gsc.common.exception.ContractExeException;
+import org.gsc.common.exception.ContractValidateException;
+import org.gsc.common.exception.DupTransactionException;
 import org.gsc.common.exception.GscException;
 import org.gsc.common.exception.HeaderNotFound;
 import org.gsc.common.exception.NonCommonBlockException;
 import org.gsc.common.exception.StoreException;
+import org.gsc.common.exception.TaposException;
+import org.gsc.common.exception.TooBigTransactionException;
+import org.gsc.common.exception.TransactionExpirationException;
 import org.gsc.common.exception.UnLinkedBlockException;
+import org.gsc.common.exception.ValidateScheduleException;
+import org.gsc.common.exception.ValidateSignatureException;
 import org.gsc.common.utils.Sha256Hash;
 import org.gsc.core.chain.BlockId;
 import org.gsc.core.wrapper.BlockWrapper;
 import org.gsc.core.wrapper.TransactionWrapper;
+import org.gsc.db.Manager;
 import org.gsc.net.message.MessageTypes;
 import org.gsc.net.message.gsc.GscMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ChainControllerImpl implements ChainController {
 
+  @Autowired
+  private Manager dbManager;
+
   @Override
-  public LinkedList<Sha256Hash> handleBlock(BlockWrapper block, boolean syncMode)
+  public synchronized LinkedList<Sha256Hash> handleBlock(BlockWrapper block, boolean syncMode)
       throws BadBlockException, UnLinkedBlockException, InterruptedException, NonCommonBlockException {
-    return null;
+
+    if (block.getInstance().getSerializedSize() > BLOCK_SIZE + 100) {
+      throw new BadBlockException("block size over limit");
+    }
+
+    // TODO timestamp should be consistent.
+    long gap = block.getTimeStamp() - System.currentTimeMillis();
+    if (gap >= BLOCK_PRODUCED_INTERVAL) {
+      throw new BadBlockException("block time error");
+    }
+    try {
+      dbManager.preValidateTransactionSign(block);
+      dbManager.pushBlock(block);
+      if (!syncMode) {
+        List<TransactionWrapper> trx = null;
+        trx = block.getTransactions();
+        return trx.stream()
+            .map(TransactionWrapper::getTransactionId)
+            .collect(Collectors.toCollection(LinkedList::new));
+      } else {
+        return null;
+      }
+
+    } catch (AccountResourceInsufficientException e) {
+      throw new BadBlockException("AccountResourceInsufficientException," + e.getMessage());
+    } catch (ValidateScheduleException e) {
+      throw new BadBlockException("validate schedule exception," + e.getMessage());
+    } catch (ValidateSignatureException e) {
+      throw new BadBlockException("validate signature exception," + e.getMessage());
+    } catch (ContractValidateException e) {
+      throw new BadBlockException("ContractValidate exception," + e.getMessage());
+    } catch (ContractExeException e) {
+      throw new BadBlockException("Contract Execute exception," + e.getMessage());
+    } catch (TaposException e) {
+      throw new BadBlockException("tapos exception," + e.getMessage());
+    } catch (DupTransactionException e) {
+      throw new BadBlockException("DupTransaction exception," + e.getMessage());
+    } catch (TooBigTransactionException e) {
+      throw new BadBlockException("TooBigTransaction exception," + e.getMessage());
+    } catch (TransactionExpirationException e) {
+      throw new BadBlockException("Expiration exception," + e.getMessage());
+    } catch (BadNumberBlockException e) {
+      throw new BadBlockException("bad number exception," + e.getMessage());
+    }
+
   }
 
   @Override
