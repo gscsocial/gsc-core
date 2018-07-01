0d3c4d6def6bb2756d76888cdb5891988fbbadca
EvanYip <contact@gsc.social>
2018-07-01 18:12:50 +0800

    add genesis produce

--- src/main/java/org/gsc/core/chain/Genesis.java
+++ src/main/java/org/gsc/core/chain/Genesis.java
@@ -1,5 +1,39 @@
 package org.gsc.core.chain;
 
-public class Genesis {
+import com.google.protobuf.ByteString;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.gsc.common.utils.Sha256Hash;
+import org.gsc.common.utils.TransactionUtil;
+import org.gsc.config.Args;
+import org.gsc.config.args.GenesisBlock;
+import org.gsc.core.wrapper.BlockWrapper;
+import org.gsc.protos.Protocol.Transaction;
 
+public class Genesis {
+  public static BlockWrapper newGenesisBlock() {
+
+    Args args = Args.getInstance();
+    GenesisBlock genesisBlockArg = args.getGenesisBlock();
+    List<Transaction> transactionList =
+        genesisBlockArg.getAssets().stream()
+            .map(key -> {
+              byte[] address = key.getAddress();
+              long balance = key.getBalance();
+              return TransactionUtil.newGenesisTransaction(address, balance);
+            })
+            .collect(Collectors.toList());
+
+    long timestamp = Long.parseLong(genesisBlockArg.getTimestamp());
+    Sha256Hash parentHash = Sha256Hash.wrap(genesisBlockArg.getParentHash().getBytes());
+    long number = Long.parseLong(genesisBlockArg.getNumber());
+
+    BlockWrapper blockCapsule = new BlockWrapper(timestamp, parentHash, number,
+        ByteString.copyFromUtf8("0x00"), transactionList);
+
+    blockCapsule.setMerkleRoot();
+    blockCapsule.generatedByMyself = true;
+
+    return blockCapsule;
+  }
 }
