21707a4ab6b7bcd8681350882b449990282b78c2
zhaixuefei <zhaixuefei@micous.com>
2018-08-06 17:51:15 +0800

    modify logback config file

--- src/test/java/org/gsc/common/command/CliTest.java
+++ src/test/java/org/gsc/common/command/CliTest.java
@@ -19,9 +19,4 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class CliTest {
-  //      @Test
-  //      public void testCli() {
-  //          Cli cli = new Cli();
-  //          cli.run(Peer.getInstance(Peer.PEER_NORMAL));
-  //      }
 }
