2fa51f482db3710b4de2d8b9e9e8355d068c1aac
kay <kayfhan@sina.com>
2018-09-05 20:39:01 +0800

    ws

---
+++ src/test/java/org/gsc/core/wrapper/VotesCapsuleTest.java
@@ -0,0 +1,7 @@
+package org.gsc.core.wrapper;
+
+public class VotesCapsuleTest {
+
+  private static String dbPath = "output_votesCapsule_test";
+  // TODO:add unittest
+}
---
+++ src/test/java/org/gsc/services/RpcApiServiceTest.java
@@ -0,0 +1,5 @@
+package org.gsc.services;
+
+public class RpcApiServiceTest {
+
+}
