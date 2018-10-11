91e3fb4addeba8968e3e2a149688a3f30c59f933
zhaixuefei <zhaixuefei@micous.com>
2018-10-11 11:18:26 +0800

    update config to mainnet config

--- src/test/java/org/gsc/common/crypto/ECKeyTest.java
+++ src/test/java/org/gsc/common/crypto/ECKeyTest.java
@@ -21,9 +21,13 @@ public class ECKeyTest {
     assertEquals(Wallet.getAddressPreFixString() + "125b6c87b3d67114b3873977888c34582f27bbb0",
         ByteArray.toHexString(key.getAddress()));
 
-    ECKey ecKey = new ECKey(Utils.getRandom());
-    logger.info(ByteArray.toHexString(ecKey
-            .getAddress()));
+    //ECKey ecKey = new ECKey(Utils.getRandom());
+    ECKey ecKey = ECKey.fromPrivate(Hex.decode("d4ab0e3d777eb46ee138177b6a3c18a808b958d71474ed9399633f28737dc99d"));
+    byte[] priKey = ecKey.getPrivKeyBytes();
+    byte[] address = ecKey.getAddress();
+    String addressStr = Wallet.encode58Check(address); //GKuH5fbaTVNZSyNsLm9JndCEftjkAhCCMg
+    String priKeyStr = org.apache.commons.codec.binary.Hex.encodeHexString(priKey);
+    logger.info(addressStr+" "+priKeyStr);
 
   }
 }
