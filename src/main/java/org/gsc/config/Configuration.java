60f360244d80468dc920f740ceb24d6e26cbfb28
lizizj <45927991+lizizj@users.noreply.github.com>
2018-12-19 15:02:53 +0800

    Update Configuration.java

--- src/main/java/org/gsc/config/Configuration.java
+++ src/main/java/org/gsc/config/Configuration.java
@@ -2,16 +2,6 @@
  * Copyright (c) [2016] [ <ether.camp> ]
  * This file is part of the ethereumJ library.
  *
- * The ethereumJ library is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * The ethereumJ library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the ethereumJ library. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -32,12 +22,6 @@ public class Configuration {
 
   private static com.typesafe.config.Config config;
 
-  /**
-   * Get configuration by a given path.
-   *
-   * @param confFileName path to configuration file
-   * @return loaded configuration
-   */
   public static com.typesafe.config.Config getByFileName(final String shellConfFileName, final String confFileName) {
     if (isNoneBlank(shellConfFileName)) {
       File shellConfFile = new File(shellConfFileName);
